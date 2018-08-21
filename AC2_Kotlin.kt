0ee18966f8b839e4c90cd94d1f14ce9e5a4227ac
Ana Paula Lopes <30847848+AnaToco@users.noreply.github.com>
2018-08-20 21:28:30 -0300

    Update AC2_Kotlin.kt

--- AC2_Kotlin.kt
+++ AC2_Kotlin.kt
@@ -13,3 +13,23 @@ fun main(args: Array<String>) {
     println(ints.sum())
 }
 
+
+
+/*2) Escreva um programa em Kotlin com uma função que recebe por parâmetro uma lista de
+números inteiros e um número inteiro qualquer X e retorna a quantidade de vezes que X
+encontra-se na lista.*/
+
+fun conta (lista: List<Int>,  numero: Int): Int{
+    var  cont:Int = 0    
+    for (i in lista){
+        if (i == numero){
+            cont = cont +1
+        }
+    }
+    return cont
+}
+fun main(args: Array<String>) {
+    val ints = listOf<Int>(1,2,3,4,5,3,3,4,2)
+    println (conta(ints,3))
+}
+
